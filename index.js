deb28ad722334e97291f6f0a3a4d10d8a386ab74
siddhant235 <siddhant235@gmail.com>
2023-05-16 15:54:55 +0530

    added pixel

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log("just a health check")
+const scripts=document.scripts;
+const loadjs=document.querySelector('script[src*="load.js"]')
+console.log("load js src",loadjs.src.split("?"))
+console.log("scripts collection",scripts,loadjs)
+
